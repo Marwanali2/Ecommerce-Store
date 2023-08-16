ebb94b35c9a2df0976908d3ef38ae17d07557807
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2023-08-17 00:56:43 +0300

    add indicators to banners

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,40 +1,44 @@
+import 'dart:async';
+
 import 'package:ecommerce/core/utils/colors.dart';
 import 'package:ecommerce/core/widgets/custom_error_widget.dart';
 import 'package:ecommerce/features/home/presentation/managers/banner_cubit/banner_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_swiper_view/flutter_swiper_view.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({Key? key}) : super(key: key);
 
+  @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<BannerCubit, BannerState>(
       builder: (context, state) {
-        var cubit=BlocProvider.of<BannerCubit>(context);
+        var cubit = BlocProvider.of<BannerCubit>(context);
+        final pageController=PageController();
         if (state is BannerSuccess) {
           return Column(
             children: [
-              //EnjoyBar(context), // app bar
               const SizedBox(
                 height: 15,
               ),
               SizedBox(
                 height: 200,
-                child: Swiper(
-                  autoplay: true,
-                  autoplayDelay: 3000,
-                  curve: Curves.easeInOutBack,
-                  viewportFraction: 0.7,
-                  scale: 0.75,
+                child: PageView.builder(
+                  controller: pageController,
                   itemCount: cubit.bannersList!.length,
                   scrollDirection: Axis.horizontal,
                   physics: const BouncingScrollPhysics(),
                   itemBuilder: (context, index) {
                     return Padding(
                       padding: const EdgeInsets.symmetric(
-                        horizontal: 5,
+                        horizontal: 10,
                       ),
                       child: SizedBox(
                         height: 200,
@@ -50,13 +54,26 @@ class HomeViewBody extends StatelessWidget {
                     );
                   },
                 ),
-              )
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Center(
+                child: SmoothPageIndicator(
+                  controller: pageController,
+                  count: cubit.bannersList!.length,
+                  axisDirection: Axis.horizontal,
+                  effect:  ScrollingDotsEffect(
+                    radius: 5,
+                  ),
+                ),
+              ),
             ],
           );
-        } else if (state is BannerFailure) {
+        }
+        else if (state is BannerFailure) {
           return Column(
             children: [
-              //EnjoyBar(context), // app bar
               const SizedBox(
                 height: 15,
               ),
@@ -93,7 +110,6 @@ class HomeViewBody extends StatelessWidget {
         } else {
           return Column(
             children: [
-              //EnjoyBar(context), // app bar
               const SizedBox(
                 height: 15,
               ),
@@ -118,8 +134,8 @@ class HomeViewBody extends StatelessWidget {
                         width: 350,
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(18.0),
-                          child: const Center(
-                              child: CircularProgressIndicator()),
+                          child:
+                              const Center(child: CircularProgressIndicator()),
                         ),
                       ),
                     );
@@ -130,8 +146,8 @@ class HomeViewBody extends StatelessWidget {
           );
         }
       },
+
     );
+
   }
-
-
 }
