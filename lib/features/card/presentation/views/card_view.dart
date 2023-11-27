c70025655cd0fdf25c1c6c30a04da764cbd803a6
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2023-11-27 21:48:38 +0200

    finish app & fix bugs

--- lib/features/card/presentation/views/card_view.dart
+++ lib/features/card/presentation/views/card_view.dart
@@ -6,7 +6,7 @@ class CardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: CardViewBody(),
     );
   }
